519bc996f6fe040bfa4bbca8dd230748a2c2cf67
knightian <ian@whiteknightit.com.au>
2022-08-29 01:58:53 +1000

    rejig js api to provide json and status code

--- js/app.js
+++ js/app.js
@@ -276,7 +276,7 @@ async function CallToAction(currentUrl)
       window.location.replace(currentUrl);
       break;
     case '/403' || '/403/':
-      window.location.href=currentUrl;
+      //window.location.href=currentUrl;
       break;
     default:
       // code block
