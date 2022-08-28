aebd96d11442758c8a2d35cdc101eee2d1a897e1
knightian <ian@whiteknightit.com.au>
2022-08-29 00:00:47 +1000

    swagger customisation

--- swagger/js/swagger-customisation.js
+++ swagger/js/swagger-customisation.js
@@ -3,7 +3,7 @@
         setTimeout(function () {
             var logo = document.getElementsByClassName('link'); //For Changing The Link On The Logo Image
             logo[0].children[0].alt = "FFPP Swagger";
-            logo[0].children[0].src = "/swagger/img/swagger.svg"; //For Changing The Logo Image
+            logo[0].children[0].src = "/swagger-customisation/img/swagger.svg"; //For Changing The Logo Image
         });
     });
 })();
