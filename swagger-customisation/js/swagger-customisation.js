6a54134a3386502ad736a1c8b3accf017483f75a
knightian <ian@whiteknightit.com.au>
2022-08-29 10:53:11 +1000

    rejig js api to provide json and status code

--- swagger-customisation/js/swagger-customisation.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    window.addEventListener("load", function () {
-        setTimeout(function () {
-            var logo = document.getElementsByClassName('link'); //For Changing The Link On The Logo Image
-            logo[0].children[0].alt = "FFPP Swagger";
-            logo[0].children[0].src = "/swagger/img/swagger.svg"; //For Changing The Logo Image
-        });
-    });
-})();
